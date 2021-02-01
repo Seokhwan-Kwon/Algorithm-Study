91c066df54f17ebbce7c5ef4db48ce9d0c1db783
Seokhwan-Kwon <sychar05@gmail.com>
2021-02-01 23:30:48 +0900

    2021-02-01

--- 00_권석환/Week3/7568.cpp
+++ 00_권석환/Week3/7568.cpp
@@ -5,6 +5,36 @@
 //  Created by Seokhwan Kwon on 2021/01/29.
 //
 
-#include <iostream>
+#include<iostream>
 using namespace std;
-
+ 
+pair<int, int>people[50];
+int Rank[50];
+ 
+void compareRank(const int N) {
+    for (int i = 0;i < N;i++) {
+        for (int j = 0;j < N;j++) {
+            if (people[i].first > people[j].first && people[i].second > people[j].second)
+                Rank[j]++;
+        }
+    }
+}
+ 
+void printRank(const int N) {
+    for (int i = 0;i < N;i++) {
+        cout << Rank[i] + 1 << " ";
+    }
+}
+ 
+int main() {
+    int N;
+    cin >> N;
+    for (int i = 0;i < N;i++) {
+        cin >> people[i].first >> people[i].second;
+    }
+ 
+    compareRank(N);
+    printRank(N);
+ 
+    return 0;
+}
