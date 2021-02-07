36b00274d9d134620d831eac846aa49e5a10400c
Seokhwan-Kwon <sychar05@gmail.com>
2021-02-07 21:49:17 +0900

    week4

---
+++ 00_권석환/week4/1010.cpp
@@ -0,0 +1,19 @@
+//
+//  1010.cpp
+//  Problem_solving
+//
+//  Created by Seokhwan Kwon on 2021/02/07.
+//
+
+#include <iostream>
+using namespace std;
+
+
+//
+
+int main(int argc, const char * argv[])
+{
+    ios_base::sync_with_stdio(false);
+    
+    return 0;
+}
